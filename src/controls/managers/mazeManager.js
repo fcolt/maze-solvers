489686e4b0e7be4e6fefb350b2a92fe1c5dd8eeb
fcolt <olteanufabian99@gmail.com>
2024-03-10 21:13:42 +0200

    refactoring

--- src/controls/managers/mazeManager.js
+++ src/controls/managers/mazeManager.js
@@ -1,19 +1,25 @@
 export var MazeManager = {
   grid: null,
-  
+
   DIRECTION: {
     TOP: 2,
     RIGHT: 4,
     BOTTOM: 8,
-    LEFT: 16
+    LEFT: 16,
   },
-  
-  initialize: function(width, height) {
+
+  CELL: {
+    OPEN: 1,
+    CLOSED: 0,
+    CLEAR: -1
+  },
+
+  initialize: function (width, height) {
     MazeManager.grid = [];
-    
+
     // Initialize cells to 0.
-    for (var y=0; y<height; y++) {
-      for (var x=0; x<width; x++) {
+    for (var y = 0; y < height; y++) {
+      for (var x = 0; x < width; x++) {
         MazeManager.grid[y] = MazeManager.grid[y] || [];
         MazeManager.grid[y][x] = 0;
       }
@@ -22,77 +28,163 @@ export var MazeManager = {
     MazeManager.carve(0, 0);
   },
 
-  opposite: function(direction) {
+  opposite: function (direction) {
     var result = 0;
-    
-    switch(direction) {
-     case MazeManager.DIRECTION.TOP: result = MazeManager.DIRECTION.BOTTOM; break;
-     case MazeManager.DIRECTION.RIGHT: result = MazeManager.DIRECTION.LEFT; break;
-     case MazeManager.DIRECTION.BOTTOM: result = MazeManager.DIRECTION.TOP; break;
-     case MazeManager.DIRECTION.LEFT: result = MazeManager.DIRECTION.RIGHT; break;
-     default: result = MazeManager.DIRECTION.RIGHT; break;
-    };
-    
+
+    switch (direction) {
+      case MazeManager.DIRECTION.TOP:
+        result = MazeManager.DIRECTION.BOTTOM;
+        break;
+      case MazeManager.DIRECTION.RIGHT:
+        result = MazeManager.DIRECTION.LEFT;
+        break;
+      case MazeManager.DIRECTION.BOTTOM:
+        result = MazeManager.DIRECTION.TOP;
+        break;
+      case MazeManager.DIRECTION.LEFT:
+        result = MazeManager.DIRECTION.RIGHT;
+        break;
+      default:
+        result = MazeManager.DIRECTION.RIGHT;
+        break;
+    }
+
     return result;
   },
-  
-  path: function(fromX, fromY, toX, toY, direction) {
+
+  path: function (fromX, fromY, toX, toY, direction) {
     MazeManager.grid[fromY][fromX] |= direction;
     MazeManager.grid[toY][toX] |= MazeManager.opposite(direction);
-    
+
     // Carve a path from the new cell.
     MazeManager.carve(toX, toY);
   },
-  
-  carve: function(x, y) {
+
+  carve: function (x, y) {
     // Carve a path from x,y to an adjacent cell.
-    
+
     // First, try each direction (in random order).
     var directions = [0, 1, 2, 3].sort(() => Math.random() * 2 - 1);
 
-    directions.forEach(function(direction) {
-      if (direction === 0 && y-1 >= 0 && MazeManager.grid[y-1][x] === 0) {
+    directions.forEach(function (direction) {
+      if (direction === 0 && y - 1 >= 0 && MazeManager.grid[y - 1][x] === 0) {
         // Top.
         MazeManager.path(x, y, x, y - 1, MazeManager.DIRECTION.TOP);
-      }
-      else if (direction === 1 && x + 1 < MazeManager.grid[0].length && MazeManager.grid[y][x+1] === 0) {
+      } else if (
+        direction === 1 &&
+        x + 1 < MazeManager.grid[0].length &&
+        MazeManager.grid[y][x + 1] === 0
+      ) {
         // Right.
         MazeManager.path(x, y, x + 1, y, MazeManager.DIRECTION.RIGHT);
-      }
-      else if (direction === 2 && y + 1 < MazeManager.grid.length && MazeManager.grid[y+1][x] === 0) {
+      } else if (
+        direction === 2 &&
+        y + 1 < MazeManager.grid.length &&
+        MazeManager.grid[y + 1][x] === 0
+      ) {
         // Bottom.
         MazeManager.path(x, y, x, y + 1, MazeManager.DIRECTION.BOTTOM);
-      }
-      else if (direction === 3 && x - 1 >= 0 && MazeManager.grid[y][x-1] === 0) {
+      } else if (
+        direction === 3 &&
+        x - 1 >= 0 &&
+        MazeManager.grid[y][x - 1] === 0
+      ) {
         // Left.
         MazeManager.path(x, y, x - 1, y, MazeManager.DIRECTION.LEFT);
       }
     });
   },
 
-  toString: function() {
-    var result = '';
+  toString: function () {
+    var result = "";
 
     // Top border.
-    for (var i=0; i<MazeManager.grid[0].length * 2; i++) {
-      result += '_';
+    for (var i = 0; i < MazeManager.grid[0].length * 2; i++) {
+      result += "_";
     }
 
-    result += '\n';
+    result += "\n";
 
     // Main content.
-    for (var y=0; y<MazeManager.grid.length; y++) {
-      result += '|';
+    for (var y = 0; y < MazeManager.grid.length; y++) {
+      result += "|";
 
-      for (var x=0; x<MazeManager.grid[0].length; x++) {
+      for (var x = 0; x < MazeManager.grid[0].length; x++) {
         // We only need to check south and east (because north and west have borders already included).
-        result += (MazeManager.grid[y][x] & MazeManager.DIRECTION.BOTTOM) === MazeManager.DIRECTION.BOTTOM ? ' ' : '_';
-        result += (MazeManager.grid[y][x] & MazeManager.DIRECTION.RIGHT) === MazeManager.DIRECTION.RIGHT ? ' ' : '|';
+        result +=
+          (MazeManager.grid[y][x] & MazeManager.DIRECTION.BOTTOM) ===
+          MazeManager.DIRECTION.BOTTOM
+            ? " "
+            : "_";
+        result +=
+          (MazeManager.grid[y][x] & MazeManager.DIRECTION.RIGHT) ===
+          MazeManager.DIRECTION.RIGHT
+            ? " "
+            : "|";
       }
 
-      result += '\n';
+      result += "\n";
     }
 
     return result;
+  },
+
+  gridElements: function() {
+    var elements = [];
+
+    // Top border.
+    for (var i = 0; i <= MazeManager.grid[0].length * 2; i++) {
+      elements.push(MazeManager.CELL.CLOSED);
+    }
+
+    elements.push(MazeManager.CELL.CLEAR);
+
+    // Main grid.
+    for (var y = 0; y < MazeManager.grid.length; y++) {
+      var passageRow = [];
+
+      // Left border column.
+      elements.push(MazeManager.CELL.CLOSED);
+      passageRow.push(MazeManager.CELL.CLOSED);
+
+      // Rooms. Note, we only need to check south and east (because north and west have borders already included).
+      for (var x = 0; x < MazeManager.grid[0].length; x++) {
+        // Add a cell for the room.
+        elements.push(MazeManager.CELL.OPEN);
+
+        if (
+          (MazeManager.grid[y][x] & MazeManager.DIRECTION.BOTTOM) ===
+          MazeManager.DIRECTION.BOTTOM
+        ) {
+          // Open a passage to the south.
+          passageRow.push(MazeManager.CELL.OPEN);
+        } else {
+          // Close a passage to the south.
+          passageRow.push(MazeManager.CELL.CLOSED);
+        }
+
+        // Add closed passage to next row between rooms.
+        passageRow.push(MazeManager.CELL.CLOSED);
+
+        if (
+          (MazeManager.grid[y][x] & MazeManager.DIRECTION.RIGHT) ===
+          MazeManager.DIRECTION.RIGHT
+        ) {
+          // Open a passage to the east.
+          elements.push(MazeManager.CELL.OPEN);
+        } else {
+          // Close a passage to the east.
+          elements.push(MazeManager.CELL.CLOSED);
+        }
+      }
+
+      elements.push(MazeManager.CELL.CLEAR);
+      passageRow.push(MazeManager.CELL.CLEAR);
+
+      // Append passages row to elements.
+      elements.push.apply(elements, passageRow);
+    }
+
+    return elements;
   }
 };
--- src/controls/mazeControl.js
+++ src/controls/mazeControl.js
@@ -1,69 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { MazeManager } from "./managers/mazeManager.js";
 
+const CELL_TYPE_TO_JSX = {
+  [MazeManager.CELL.OPEN]: <div className="cell open"></div>,
+  [MazeManager.CELL.CLOSED]: <div className="cell closed"></div>,
+  [MazeManager.CELL.CLEAR]: <div className="clear"></div>,
+};
+
 const MazeControl = (props) => {
   const [grid, setGrid] = useState([]);
   const [width, setWidth] = useState(props.width);
   const [height, setHeight] = useState(props.height);
-
-  const gridElements = () => {
-    const elements = [];
-
-    // Top border.
-    for (let i = 0; i <= MazeManager.grid[0].length * 2; i++) {
-      elements.push(<div className="cell closed"></div>);
-    }
-
-    elements.push(<div className="clear"></div>);
-
-    // Main grid.
-    for (let y = 0; y < MazeManager.grid.length; y++) {
-      const passageRow = [];
-
-      // Left border column.
-      elements.push(<div className="cell closed"></div>);
-      passageRow.push(<div className="cell closed"></div>);
-
-      // Rooms. Note, we only need to check south and east (because north and west have borders already included).
-      for (let x = 0; x < MazeManager.grid[0].length; x++) {
-        // Add a cell for the room.
-        elements.push(<div className="cell open"></div>);
-
-        if (
-          (MazeManager.grid[y][x] & MazeManager.DIRECTION.BOTTOM) ===
-          MazeManager.DIRECTION.BOTTOM
-        ) {
-          // Open a passage to the south.
-          passageRow.push(<div className="cell open"></div>);
-        } else {
-          // Close a passage to the south.
-          passageRow.push(<div className="cell closed"></div>);
-        }
-
-        // Add closed passage to next row between rooms.
-        passageRow.push(<div className="cell closed"></div>);
-
-        if (
-          (MazeManager.grid[y][x] & MazeManager.DIRECTION.RIGHT) ===
-          MazeManager.DIRECTION.RIGHT
-        ) {
-          // Open a passage to the east.
-          elements.push(<div className="cell open"></div>);
-        } else {
-          // Close a passage to the east.
-          elements.push(<div className="cell closed"></div>);
-        }
-      }
-
-      elements.push(<div className="clear"></div>);
-      passageRow.push(<div className="clear"></div>);
-
-      // Append passages row to elements.
-      elements.push.apply(elements, passageRow);
-    }
-
-    return elements;
-  };
 
   const redraw = () => {
     MazeManager.initialize(width, height);
@@ -75,7 +22,11 @@ const MazeControl = (props) => {
     if (props.type.toLowerCase() === "ascii") {
       setGrid(MazeManager.toString());
     } else {
-      setGrid(gridElements());
+      setGrid(
+        MazeManager.gridElements().map((cell) => {
+          return CELL_TYPE_TO_JSX[cell];
+        })
+      );
     }
   };
 
